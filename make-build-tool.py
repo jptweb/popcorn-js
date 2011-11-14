32c247792e78f24388c74b04bc4669fa4b99c608
Christopher De Cairos <chris.decairos@gmail.com>
2011-11-14 11:38:29 -0500

    restructuring for build tool files and dependencies

---
+++ make-build-tool.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+import sys, os, os.path, markdown, elementtree, re, shutil
+from subprocess import call
+
+dist = "./dist/build-tool"
+addonsdir = "./addons"
+plugindist = dist + "/plugins"
+parserdist = dist + "/parsers"
+playerdist = dist + "/players"
+moduledist = dist + "/modules"
+effectdist = dist + "/effects"
+
+manifestText = "{"
+
+if not os.path.exists( "./dist" ):
+  os.mkdir( "./dist" )
+
+if not os.path.exists( dist ):
+  os.mkdir( dist )
+
+if not os.path.exists( parserdist ):
+  os.mkdir( parserdist )
+
+if not os.path.exists( plugindist ):
+  os.mkdir( plugindist )
+
+if not os.path.exists( playerdist ):
+  os.mkdir( playerdist )
+
+if not os.path.exists( moduledist ):
+  os.mkdir( moduledist )
+
+if not os.path.exists( effectdist ):
+  os.mkdir( effectdist )
+
+def safePath( path ):
+  return path.replace( "\\", "/" );
+
+def checkPath( path ):
+  if not os.path.exists( path ):
+    os.mkdir( path )
+
+def process( root, filename, dist, m ):
+  if filename.endswith( ".md" ):
+    md = markdown.Markdown()
+    fullpath = safePath( os.path.abspath( os.path.join( root, filename )))
+    FILE = open( fullpath, "r" )
+    newfilename = filename.replace( ".md", ".html" )
+    folder = os.path.split( os.path.dirname( fullpath ) )[ 1 ]
+    distpath = safePath( os.path.abspath( os.path.join( dist, folder ) ) )
+
+    html = md.convert( FILE.read() )
+    FILE.close()
+
+    checkPath( distpath )
+
+    distpath = safePath( os.path.abspath( os.path.join( distpath, newfilename ) ) )
+    FILE = open( distpath, "w" )
+
+    FILE.write( html )
+    FILE.close()
+    return ""
+
+  if filename.endswith( ".js" ) \
+      and not filename.endswith( ".unit.js" ) \
+      and not filename.endswith( ".player.api.js" ):
+
+    fullpath = safePath( os.path.abspath( os.path.join( root, filename ) ) )
+    folder = os.path.split( os.path.dirname( fullpath ) )[ 1 ]
+    distpath = safePath( os.path.abspath( os.path.join( dist, folder ) ) )
+    checkPath( distpath )
+
+    shutil.copy( fullpath, distpath )
+
+    description = "Placeholder Description"
+
+    if os.path.isfile( root + "/readme.md"):
+      FILE = open( root + "/readme.md", "r" )
+      found = False
+      skipped = False
+      for line in FILE:
+        if found == True:
+          if skipped == True:
+            description = line[:len(line) - 1]
+            break
+          else:
+            skipped = True
+        if re.search( "Purpose", line ):
+          found = True
+      FILE.close()
+
+    return '    { "name": "' + folder + '/' + filename + '", "description": "' + description +'" }, \n'
+
+print "Building dist/plugins ..."
+print os.path.exists(addonsdir + "/plugins")
+manifestText = manifestText + '\n  "plugins": [\n'
+for root, dirs, filenames in os.walk( addonsdir + "/plugins" ):
+  for filename in filenames:
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    text = process( root, filename, plugindist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
+
+print "Building dist/parsers/ ..."
+manifestText = manifestText[:len( manifestText ) - 3 ]  + '\n  ],\n  "parsers": [\n'
+for root, dirs, filenames in os.walk( addonsdir + "/parsers" ):
+  for filename in filenames:
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    text = process( root, filename, parserdist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
+
+print "Building dist/players/ ..."
+manifestText = manifestText[:len( manifestText ) - 3 ]  + '\n  ],\n  "players": [\n'
+for root, dirs, filenames in os.walk( addonsdir + "/players" ):
+  for filename in filenames:
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    text = process( root, filename, playerdist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
+
+print "Building dist/modules/ ..."
+manifestText = manifestText[:len( manifestText ) - 3 ]  + '\n  ],\n  "modules": [\n'
+for root, dirs, filenames in os.walk( addonsdir + "/modules" ):
+  for filename in filenames:
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    text = process( root, filename, moduledist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
+
+print "Building dist/effects/ ..."
+manifestText = manifestText[:len( manifestText ) - 3 ]  + '\n  ], \n  "effects": [\n'
+for root, dirs, filenames in os.walk( addonsdir + "/effects" ):
+  for filename in filenames:
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    text = process( root, filename, effectdist, manifestText )
+    if ( text ):
+      manifestText = manifestText + text
+
+manifestText = manifestText[:len( manifestText ) - 3 ]  + '\n  ]\n}'
+
+if os.path.isfile( "./manifest.json" ):
+  os.remove( "./manifest.json" )
+
+print "writing new manifest.json"
+manifest = open( "./manifest.json", "w" )
+manifest.write( manifestText )
+manifest.close()
